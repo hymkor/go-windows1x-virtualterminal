4e4e85c44316e3c35f7c357e5f39a04a54557062
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-05-06 11:30:40 +0900

    Add example for EnableVirtualTerminalInput

---
+++ examples/example2.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"golang.org/x/term"
+
+	"github.com/hymkor/go-windows10-ansi"
+)
+
+func mains() error {
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		return err
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	disable, err := ansi.EnableStdin()
+	if err != nil {
+		return err
+	}
+	defer disable()
+
+	terminal := term.NewTerminal(&struct {
+		io.Reader
+		io.Writer
+	}{os.Stdin, os.Stdout}, "> ")
+
+	line, err := terminal.ReadLine()
+	if err != nil {
+		return err
+	}
+	fmt.Println("Line:", line)
+	return nil
+}
+
+func main() {
+	if err := mains(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/hymkor/go-windows10-ansi
 go 1.20
 
 require golang.org/x/sys v0.8.0
+
+require golang.org/x/term v0.8.0 // indirect
