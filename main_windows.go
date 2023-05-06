d8e2c87d6b2178b900e903e1063e8d4072a9eac9
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-05-06 12:15:54 +0900

    Remove ModeOp

--- main_windows.go
+++ main_windows.go
@@ -9,62 +9,29 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-type ModeOp interface {
-	Op(mode uint32) uint32
-}
+const _PARAMETER_IS_INCORRECT = 87
 
-type ModeReset uint32
-
-func (this ModeReset) Op(mode uint32) uint32 {
-	return mode &^ uint32(this)
-}
-
-type ModeSet uint32
-
-func (this ModeSet) Op(mode uint32) uint32 {
-	return mode | uint32(this)
-}
-
-func changeConsoleMode(console windows.Handle, ops ...ModeOp) (func(), error) {
+func changeConsoleMode(console windows.Handle, bits uint32) (func(), error) {
 	var mode uint32
 	err := windows.GetConsoleMode(console, &mode)
 	if err != nil {
 		return func() {}, err
 	}
-	restore := func() { windows.SetConsoleMode(console, mode) }
-
-	if len(ops) > 0 {
-		newMode := mode
-		for _, op1 := range ops {
-			newMode = op1.Op(newMode)
-		}
-		err = windows.SetConsoleMode(console, newMode)
+	err = windows.SetConsoleMode(console, mode|bits)
+	if errno, ok := err.(syscall.Errno); ok && errno == _PARAMETER_IS_INCORRECT {
+		err = ErrNotSupported
 	}
-	return restore, err
+	return func() { windows.SetConsoleMode(console, mode) }, err
 }
-
-const _PARAMETER_IS_INCORRECT = 87
 
 func enableVirtualTerminalInput() (func(), error) {
-	f, err := changeConsoleMode(windows.Stdin, ModeSet(windows.ENABLE_VIRTUAL_TERMINAL_INPUT))
-	if errno, ok := err.(syscall.Errno); ok && errno == _PARAMETER_IS_INCORRECT {
-		return f, ErrNotSupported
-	}
-	return f, err
-}
-
-func enableVirtualTerminalProcessing(h windows.Handle) (func(), error) {
-	f, err := changeConsoleMode(h, ModeSet(windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING))
-	if errno, ok := err.(syscall.Errno); ok && errno == _PARAMETER_IS_INCORRECT {
-		return f, ErrNotSupported
-	}
-	return f, err
+	return changeConsoleMode(windows.Stdin, windows.ENABLE_VIRTUAL_TERMINAL_INPUT)
 }
 
 func enableStdoutVirtualTerminalProcessing() (func(), error) {
-	return enableVirtualTerminalProcessing(windows.Stdout)
+	return changeConsoleMode(windows.Stdout, windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
 }
 
 func enableStderrVirtualTerminalProcessing() (func(), error) {
-	return enableVirtualTerminalProcessing(windows.Stderr)
+	return changeConsoleMode(windows.Stderr, windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
 }
